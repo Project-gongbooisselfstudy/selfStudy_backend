48e501f2cdd2df93375b21b09e7648c838092418
jjimini98 <jjimini98@naver.com>
2022-04-04 23:14:43 +0900

    0404 Question controller : 저장 기능만 구현

--- src/main/java/com/selfstudy/controller/QuestionController.java
+++ src/main/java/com/selfstudy/controller/QuestionController.java
@@ -2,49 +2,35 @@ package com.selfstudy.controller;
 
 import com.selfstudy.domain.Question;
 import com.selfstudy.service.QuestionService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.selfstudy.service.QuestionServiceImpl;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 import javax.sql.DataSource;
 
-@Controller
+@RestController
 public class QuestionController {
 
-
 	private JdbcTemplate jdbcTemplate;
+    private QuestionServiceImpl questionServiceIm;
+    private Question question;
 
-	public QuestionController(DataSource dataSource) {
-		this.jdbcTemplate = new JdbcTemplate(dataSource);
-	}
-//    @Autowired
-//    private final QuestionService questionService;
-//    private final Question question;
-//
-//    public QuestionController(QuestionService questionService, Question question) {
-//        this.questionService = questionService;
-//        this.question = question;
-//    }
+    public QuestionController(JdbcTemplate jdbcTemplate, QuestionServiceImpl questionServiceIm, Question question) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.questionServiceIm = questionServiceIm;
+        this.question = question;
+    }
 
-    // private String question;
-    // private String answer;
-    // private String classification;
-
-    @GetMapping("create")
-    public String createForm(@RequestParam String question ,@RequestParam String answer ,@RequestParam String classification){
-
-        System.out.println("===값 전달받기 === ");
-        String sql = "INSERT INTO testQuestion(question, answer, classification) VALUES (?,?,?)";
-        Object[] Params = {question, answer, classification};
-
-        jdbcTemplate.update(sql,Params);
-
-        System.out.println("=======된건가..?=======");
-        return "questionForm";
+    @RequestMapping(value = "question/create", produces = "application/json; charset=UTF-8")
+    public Question save() {
+        Question qu = new Question();
+        qu.setId(2);
+        qu.setContents("test이지롱");
+        qu.setAnswer("db이지롱");
+        qu.setClassification("test");
+        questionServiceIm.saveQuestion(qu);
+        System.out.println("=========controller : 저장=========");
+        return qu;
     }
 
 
