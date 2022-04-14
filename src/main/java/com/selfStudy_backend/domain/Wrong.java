70f4a52d5cc77d3eaf639648abd9eaed317cdd6b
jjimini98 <jjimini98@naver.com>
2022-04-14 21:37:41 +0900

    0414 오답노트 기능 구현중

---
+++ src/main/java/com/selfStudy_backend/domain/Wrong.java
@@ -0,0 +1,32 @@
+package com.selfStudy_backend.domain;
+
+public class Wrong {
+
+    private int wrong_id;
+    private int question_id;
+    private String user_id;
+
+    public int getWrong_id() {
+        return wrong_id;
+    }
+
+    public void setWrong_id(int wrong_id) {
+        this.wrong_id = wrong_id;
+    }
+
+    public int getQuestion_id() {
+        return question_id;
+    }
+
+    public void setQuestion_id(int question_id) {
+        this.question_id = question_id;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+}
---
+++ src/main/java/com/selfStudy_backend/repository/WrongRepository.java
@@ -0,0 +1,19 @@
+package com.selfStudy_backend.repository;
+
+import com.selfStudy_backend.domain.Question;
+import com.selfStudy_backend.domain.Wrong;
+
+import java.util.List;
+
+public interface WrongRepository {
+
+    // 오답인 문제만 저장
+    void saveWrong(Question question);
+
+    // 전제 오답문제 조회
+    List<Wrong> findAll();
+
+
+
+
+}
