d18035b21a0cdcad9a1f94616260bc66360ae520
jjimini98 <jjimini98@naver.com>
2022-04-19 22:14:32 +0900

    0419 cors 문제 해결

---
+++ src/main/java/com/selfStudy_backend/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.selfStudy_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// cors 설정
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*");    }
+
+
+}
