f070f20b2cf6e651943e566244a4f1601023160b
jjimini98 <jjimini98@naver.com>
2022-04-27 18:49:12 +0900

    0427 config 필요없는 부분 삭제

--- src/main/java/com/selfStudy_backend/config/SpringConfig.java
+++ src/main/java/com/selfStudy_backend/config/SpringConfig.java
@@ -1,6 +1,7 @@
 package com.selfStudy_backend.config;
 
 
+import com.selfStudy_backend.domain.CookieUser;
 import com.selfStudy_backend.domain.Question;
 import com.selfStudy_backend.domain.Wrong;
 import com.selfStudy_backend.repository.JDBCQuestionRepository;
@@ -10,7 +11,6 @@ import com.selfStudy_backend.repository.WrongRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.validation.annotation.Validated;
 
 import javax.sql.DataSource;
 
@@ -19,16 +19,11 @@ public class SpringConfig {
 
     private final DataSource dataSource;
     private  JdbcTemplate jdbcTemplate;
+    private static CookieUser cookieUser;
     public SpringConfig(DataSource dataSource){
         this.dataSource = dataSource;
     }
 
-
-//    @Bean
-//    public QuestionServiceImpl questionService() {
-//        JDBCQuestionRepository QuestionRepository;
-//        return new QuestionServiceImpl(JDBCQuestionRepository);
-//    }
 
     @Bean
     public QuestionRepository questionRepository() {
